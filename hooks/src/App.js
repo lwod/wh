49b230ba3299fca57bb9faea2e12b051ff487c6f
lwod <lwodcurrentmail@gmail.com>
2021-03-28 20:28:39 +0300

    use useRef and ref-attribute of the element to get full access to the element

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -3,15 +3,18 @@ import React, {useState, useEffect, useRef} from "react";
 function App() {
   const [value,setValue] = useState('init')
   const renderCurrent = useRef(1)
+  const inputRef = useRef(null)
+  
   
   useEffect(()=>{
     renderCurrent.current++
+    console.log(inputRef.current.value)
   })
   
   return (
     <div>
       <h1>Render counter: {renderCurrent.current}</h1>
-      <input type={'text'} onChange={event=>{setValue(event.target.value)}} value={value}/>
+      <input ref={inputRef} type={'text'} onChange={event=>{setValue(event.target.value)}} value={value}/>
     </div>
   );
 }
