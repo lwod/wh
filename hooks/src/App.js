424563d9feb9d2b3bd7eda318693fb03fadd7c1b
lwod <lwodcurrentmail@gmail.com>
2021-03-28 20:50:28 +0300

    init for useMemo

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+  
+  const [number, setNumber] = useState(24)
+  
   return (
-    <div>
+    <>
+      <h1>Counting property: {number}</h1>
+      <button className={'btn btn-success'} onClick={()=>{setNumber(prev => prev + 1)}}>Increase</button>
+      <button className={'btn btn-danger'} onClick={()=>{setNumber(prev => prev - 1)}}>Decrease</button>
     
-    </div>
+    </>
   );
 }
 
