61cb79dd2d3fa56443f1b496f2590483f7e25443
lwod <lwodcurrentmail@gmail.com>
2021-03-28 15:34:39 +0300

    basic counter

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -3,7 +3,9 @@ import React from "react";
 function App() {
   return (
     <div>
-    
+      <h1>Counter</h1>
+      <button className={"btn btn-success"}>Increase</button>
+      <button className={"btn btn-danger"}>Decrease</button>
     </div>
   );
 }
