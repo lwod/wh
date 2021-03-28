2a904a2c23721bc1c67920d835c9cc227bc42eac
lwod <lwodcurrentmail@gmail.com>
2021-03-28 19:52:50 +0300

    add event-remover for the useEffect callback function

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -6,13 +6,20 @@ function App() {
         y:0,
     })
     
+    const mouseMoveHandler = (event)=>{
+        setPos({
+            x: event.clientX,
+            y: event.clientY,
+        })
+    }
+    
     useEffect(()=>{
-       window.addEventListener('mousemove', (event)=>{
-           setPos({
-               x: event.clientX,
-               y: event.clientY,
-           })
-       })
+       window.addEventListener('mousemove', mouseMoveHandler)
+        
+        return ()=>{
+           window.removeEventListener('mousemove', mouseMoveHandler);
+        }
+       
     },[])
     
   return (
