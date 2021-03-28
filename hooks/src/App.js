9a9a156e8e8310fcd60b4af78d79d65dd6fd85f8
lwod <lwodcurrentmail@gmail.com>
2021-03-28 21:01:19 +0300

    add styles for h1-tag text color with dependencies by state colored

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -15,11 +15,15 @@ function App() {
   const [number, setNumber] = useState(24);
   const [colored, setColored] = useState(false)
   
-  const computed = complexComp(number)
+  const computed = complexComp(number);
+  
+  const styles = {
+      color: colored ? 'darkred' : 'black'
+  }
   
   return (
     <>
-      <h1>Counting property: {computed}</h1>
+      <h1 style={styles}>Counting property: {computed}</h1>
       <button className={'btn btn-success'} onClick={()=>{setNumber(prev => prev + 1)}}>Increase</button>
         <button className={'btn btn-danger'} onClick={()=>{setNumber(prev => prev - 1)}}>Decrease</button>
         <button className={'btn btn-warning'} onClick={()=>{setColored(prev => !prev)}}>Change</button>
