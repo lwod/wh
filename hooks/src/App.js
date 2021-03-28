828e4057e3d09d14a2df3c9e8f37d078074c7a5f
lwod <lwodcurrentmail@gmail.com>
2021-03-28 18:54:04 +0300

    init for useEffect hook

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+  const [type, setType] = useState('users')
+
   return (
     <div>
-    
+      <h1>Source: {type}</h1>
+      <button onClick={()=>{setType('users')}}>Users</button>
+      <button onClick={()=>{setType('todo')}}>Todo</button>
+      <button onClick={()=>{setType('posts')}}>Posts</button>
     </div>
   );
 }
