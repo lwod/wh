59f0ee7e812d4f0f11aa0b78458b8e7d463df409
lwod <lwodcurrentmail@gmail.com>
2021-03-28 15:50:38 +0300

    use useState for counter throw call functions as reaction to the events on react

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -3,11 +3,19 @@ import React, {useState} from "react";
 function App() {
     const [counter, setCounter] =  useState(0)
     
+    const increment = () =>{
+        setCounter(counter + 1)
+    }
+    
+    const decrement = () =>{
+        setCounter(counter - 1)
+    }
+    
   return (
     <div>
-      <h1>Counter</h1>
-      <button className={"btn btn-success"}>Increase</button>
-      <button className={"btn btn-danger"}>Decrease</button>
+      <h1>Counter: {counter}</h1>
+      <button onClick={increment} className={"btn btn-success"}>Increase</button>
+      <button onClick={decrement} className={"btn btn-danger"}>Decrease</button>
     </div>
   );
 }
