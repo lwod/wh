e138a740c0fcaf9af1a685381d237668fa2d5990
lwod <lwodcurrentmail@gmail.com>
2021-03-28 20:06:41 +0300

    use useRer for count number of rendering input element

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,9 +1,17 @@
 import React, {useState, useEffect, useRef} from "react";
 
 function App() {
+  const [value,setValue] = useState('init')
+  const renderCurrent = useRef(1)
+  
+  useEffect(()=>{
+    renderCurrent.current++
+  })
+  
   return (
     <div>
-    
+      <h1>Render counter: {renderCurrent.current}</h1>
+      <input type={'text'} onChange={event=>{setValue(event.target.value)}} value={value}/>
     </div>
   );
 }
