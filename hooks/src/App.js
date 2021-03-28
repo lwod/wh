edc9c3178de0db715555ce30b2502a6c690460ee
lwod <lwodcurrentmail@gmail.com>
2021-03-28 16:51:54 +0300

    change part of state-object when use useState without rewrite all object

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,26 +1,26 @@
 import React, {useState} from "react";
 
-const initCounter = () => {
-    console.log('calc counter');
-    return Math.trunc(Math.random() * 20)
-}
 
 function App() {
-    const [counter, setCounter] =  useState(initCounter)
     
-    const increment = () =>{
-        setCounter(prevCounter => prevCounter + 1)
-    }
+    const [state, setState] = useState({
+        title: 'Counter',
+        date: Date.now()
+    })
     
-    const decrement = () =>{
-        setCounter(counter - 1)
+    const updateTitle = () => {
+        setState(prev=>{
+            return {...prev, title:'new title'}
+        })
     }
     
   return (
     <div>
-      <h1>Counter: {counter}</h1>
-      <button onClick={increment} className={"btn btn-success"}>Increase</button>
-      <button onClick={decrement} className={"btn btn-danger"}>Decrease</button>
+      <h1>Counter</h1>
+
+        <button onClick={updateTitle} className={"btn btn-default"}>Change title</button>
+        
+        <pre>{JSON.stringify(state, null, 2)}</pre>
     </div>
   );
 }
