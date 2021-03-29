e9b5e62bbca03d19972c48ed14d2b250dc8be37a
lwod <lwodcurrentmail@gmail.com>
2021-03-29 12:14:33 +0300

    basic for useCallback

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+  
+  const [colored, setColored] = useState(false);
+  const [count, setCount] = useState(1);
+  
+  const styles = {color : colored ? 'darken' : 'black'}
+  
   return (
-    <div>
-    
-    </div>
+    <>
+      <h1 style={styles}>Element count: </h1>
+      <button className={'btn btn-success'} onClick={()=>setCount(prev => prev + 1)}>Increase</button>
+      <button className={'btn btn-warning'} onClick={()=>setColored(prev => !prev)}>Change</button>
+    </>
   );
 }
 
