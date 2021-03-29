9543d768c983af1cc7daf9d7ab742ce98c98ac42
lwod <lwodcurrentmail@gmail.com>
2021-03-29 12:18:00 +0300

    basic for useCallback fix 2

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -5,11 +5,11 @@ function App() {
   const [colored, setColored] = useState(false);
   const [count, setCount] = useState(1);
   
-  const styles = {color : colored ? 'darken' : 'black'}
+  const styles = {color : colored ? 'darkred' : 'black'}
   
   return (
     <>
-      <h1 style={styles}>Element count: </h1>
+      <h1 style={styles}>Element count: {count}</h1>
       <button className={'btn btn-success'} onClick={()=>setCount(prev => prev + 1)}>Increase</button>
       <button className={'btn btn-warning'} onClick={()=>setColored(prev => !prev)}>Change</button>
     </>
