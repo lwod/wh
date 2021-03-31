4ce683c7e97dc1baa96bf9932d0a29b9c9e74224
lwod <lwodcurrentmail@gmail.com>
2021-03-31 13:10:55 +0300

    add clear button for input-element

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -8,21 +8,25 @@ function useInput(initialValue){
     setValue(event.target.value)
   }
   
+  const clear = () => setValue('')
+  
   return{
-    value, onChange
+    bind: {value, onChange},
+      value,
+      clear,
   }
 }
 
 function App() {
   const input = useInput('')
-  const lastName = useInput('')
   
   return (
     <div className={'container pt-3'}>
-      <input type={'text'} {...input}/>
+      <input type={'text'} {...input.bind}/>
+      
       <h1>{input.value}</h1>
-      <input type={'text'} {...lastName}/>
-      <h1>{lastName.value}</h1>
+        
+        <button className={'btn'} onClick={()=>input.clear()}>clear</button>
     </div>
   );
 }
